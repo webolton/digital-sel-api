e15030517d67506ec516009760d0412c0844ccc1
William Bolton <william.ellet@gmail.com>
2018-06-30 17:17:27 -0400

    Add basic configuration for Devise + JWTs

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -9,7 +9,20 @@ Devise.setup do |config|
   # Devise will use the `secret_key_base` as its `secret_key`
   # by default. You can change it below and use your own secret key.
   # config.secret_key = '000169e3c06658b1967318b0a1f317b48aa63cf88e7513759e95b36389c54b1fd42ec28df1c75431c7bf86c82615a187c87ae368cfeec339f53d84b5ce8f21a0'
-  
+
+  # ==> JWT configuration
+  config.jwt do |jwt|
+    jwt.secret = ENV['DEVISE_JWT_SECRET_KEY']
+    jwt.dispatch_requests = [
+      ['POST', %r{^/login$}]
+    ]
+    jwt.revocation_requests = [
+      ['DELETE', %r{^/logout$}]
+    ]
+    jwt.expiration_time = 1.day.to_i
+  end
+  config.navigational_formats = []
+
   # ==> Controller configuration
   # Configure the parent class to the devise controllers.
   # config.parent_controller = 'DeviseController'
@@ -163,7 +176,7 @@ Devise.setup do |config|
 
   # ==> Configuration for :validatable
   # Range for password length.
-  config.password_length = 6..128
+  config.password_length = 8..128
 
   # Email regex used to validate email formats. It simply asserts that
   # one (and only one) @ exists in the given string. This is mainly
