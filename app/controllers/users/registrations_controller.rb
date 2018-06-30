dfa3f936f398a3c87bd74bafc8977cf69c04a3a2
William Bolton <william.ellet@gmail.com>
2018-06-30 17:23:45 -0400

    Basic controllers and routes for Devise + JWT

---
+++ app/controllers/users/registrations_controller.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+class Users::RegistrationsController < Devise::RegistrationsController
+  respond_to :json
+
+  def create
+    build_resource(sign_up_params)
+
+    if resource.save!
+      render json: { success: format_user(resource) }, status: 200
+    else
+      render json: { errors: resource.errors }, status: 400
+    end
+  end
+
+  private
+
+  def format_user(user)
+    { email: user.email }
+  end
+
+  # protected
+
+  # If you have extra params to permit, append them to the sanitizer.
+  # def configure_sign_up_params
+  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
+  # end
+
+  # If you have extra params to permit, append them to the sanitizer.
+  # def configure_account_update_params
+  #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
+  # end
+end
