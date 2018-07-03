f81afc6dd5b568e0d632ccdb4bd3923a03bc0d51
William Bolton <william.ellet@gmail.com>
2018-07-02 22:12:01 -0400

    Add describe block to registrations_controller_spec

--- spec/controllers/users/registrations_controller_spec.rb
+++ spec/controllers/users/registrations_controller_spec.rb
@@ -7,43 +7,45 @@ RSpec.describe Users::RegistrationsController do
     User.destroy_all
   end
 
-  subject(:do_action) { post :create, params: params }
-  let(:email) { 'user@example.com' }
-  let(:params) { { user: { email: email, password: 'password' } } }
+  describe '#register' do
+    subject(:do_action) { post :create, params: params }
+    let(:email) { 'user@example.com' }
+    let(:params) { { user: { email: email, password: 'password' } } }
 
-  context 'when user is unauthenticated' do
-    it_behaves_like 'a successful request'
+    context 'when user is unauthenticated' do
+      it_behaves_like 'a successful request'
 
-    it 'creates a new user' do
-      expect{ do_action }.to change{ User.count }.by(1)
-    end
-
-    it 'returns the correct JSON shape' do
-      do_action
-      expect(response.body).to eq({ success: { email: 'user@example.com' } }.to_json)
-    end
-
-    context 'when the email is already taken' do
-      let!(:user) { create(:user) }
-      let(:email) { user.email }
-
-      it_behaves_like 'a bad request'
-
-      it 'throws the correct error' do
-        do_action
-        expect(response.body).to eq({ errors: { email: ['Email has already been taken.'] } }.to_json)
+      it 'creates a new user' do
+        expect{ do_action }.to change{ User.count }.by(1)
       end
-    end
 
-    context 'when the parameters are empty' do
-      let(:params) { { user: { } } }
-
-      it_behaves_like 'a bad request'
-
-      it 'throws the correct error' do
+      it 'returns the correct JSON shape' do
         do_action
-        expect(response.body).to eq({ errors: { email: ['can\'t be blank'],
-                                                password: ['can\'t be blank'] } }.to_json)
+        expect(response.body).to eq({ success: { email: 'user@example.com' } }.to_json)
+      end
+
+      context 'when the email is already taken' do
+        let!(:user) { create(:user) }
+        let(:email) { user.email }
+
+        it_behaves_like 'a bad request'
+
+        it 'throws the correct error' do
+          do_action
+          expect(response.body).to eq({ errors: { email: ['Email has already been taken.'] } }.to_json)
+        end
+      end
+
+      context 'when the parameters are empty' do
+        let(:params) { { user: { } } }
+
+        it_behaves_like 'a bad request'
+
+        it 'throws the correct error' do
+          do_action
+          expect(response.body).to eq({ errors: { email: ['can\'t be blank'],
+                                                  password: ['can\'t be blank'] } }.to_json)
+        end
       end
     end
   end
