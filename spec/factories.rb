90ae6409c0a17f5afb53c2cbf6a1470c461f74a0
William Bolton <william.ellet@gmail.com>
2018-07-10 22:20:32 -0400

    Add admin factory

--- spec/factories.rb
+++ spec/factories.rb
@@ -4,7 +4,13 @@ require 'securerandom'
 
 FactoryBot.define do
   factory :user do
-    email "email#{Random.rand(10)}@email.com"
+    email    "email#{Random.rand(10)}@email.com"
     password SecureRandom.base58
+  end
+
+  factory :admin, class: User do
+    email    "admin#{Random.rand(10)}@email.com"
+    password SecureRandom.base58
+    admin    true
   end
 end
