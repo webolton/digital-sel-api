38b9e6f8bcd18694815a5bc7dba519a861f14de7
William Bolton <william.ellet@gmail.com>
2018-07-14 16:57:20 -0400

    Permit new params for user registration in Devise

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -18,16 +18,4 @@ class Users::RegistrationsController < Devise::RegistrationsController
   def format_user(user)
     { email: user.email }
   end
-
-  # protected
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_up_params
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
-  # end
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_account_update_params
-  #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
-  # end
 end
