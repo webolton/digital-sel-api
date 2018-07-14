8e6913eb83fd304539ea52fb49393d15680cfdfb
William Bolton <william.ellet@gmail.com>
2018-07-14 16:58:10 -0400

    Update User factory with new attributes

--- spec/factories.rb
+++ spec/factories.rb
@@ -4,13 +4,13 @@ require 'securerandom'
 
 FactoryBot.define do
   factory :user do
-    email    "email#{Random.rand(10)}@email.com"
-    password SecureRandom.base58
+    email      "email#{Random.rand(10)}@email.com"
+    first_name "Geoffery#{Random.rand(200)}"
+    last_name  "Chaucer#{Random.rand(200)}"
+    password   SecureRandom.base58
   end
 
   factory :admin, class: User do
-    email    "admin#{Random.rand(10)}@email.com"
-    password SecureRandom.base58
     admin    true
   end
 end
