62a9090ff3eb088bacc763437a61ca47e00c43f6
William Bolton <william.ellet@gmail.com>
2018-07-14 15:04:20 -0400

    Add validators for new models

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable, :jwt_authenticatable,
          jwt_revocation_strategy: JWTBlacklist
 
-  validates :email, presence: true, uniqueness: { message: 'Email has already been taken.' }
-  validates :password, presence: true
+  validates :email, presence:   { message: 'Email cannot be blank' },
+                    uniqueness: { message: 'Email has already been taken' }
+  validates_format_of :email, with: Devise::email_regexp, message: "Email is invalid"
+  validates :password, presence: { message: 'Password cannot be blank' }
+  validates :first_name, presence: { message: 'First name cannot be blank'}
+  validates :last_name, presence: { message: 'Last name cannot be blank' }
 end
