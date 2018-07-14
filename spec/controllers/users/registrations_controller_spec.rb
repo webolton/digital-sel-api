5e2476fd10dbe7683b1ebb0b0acecc6e94bea210
William Bolton <william.ellet@gmail.com>
2018-07-14 16:58:17 -0400

    Fix failing tests

--- spec/controllers/users/registrations_controller_spec.rb
+++ spec/controllers/users/registrations_controller_spec.rb
@@ -1,10 +1,11 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
+require 'securerandom'
 
 RSpec.describe Users::RegistrationsController do
 
-  before do
+  before(:each) do
     request.env['devise.mapping'] = Devise.mappings[:user]
     User.destroy_all
   end
@@ -12,7 +13,12 @@ RSpec.describe Users::RegistrationsController do
   describe '#register' do
     subject(:do_action) { post :create, params: params }
     let(:email) { 'user@example.com' }
-    let(:params) { { user: { email: email, password: 'password' } } }
+    let(:first_name) { 'Geoffery' }
+    let(:last_name) { 'Chaucer' }
+    let(:params) { { user: {
+                             email: email, password: 'password',
+                             first_name: first_name, last_name: last_name
+                  } } }
 
     context 'when user is unauthenticated' do
       it_behaves_like 'a successful request'
@@ -34,7 +40,7 @@ RSpec.describe Users::RegistrationsController do
 
         it 'throws the correct error' do
           do_action
-          expect(response.body).to eq({ errors: { email: ['Email has already been taken.'] } }.to_json)
+          expect(response.body).to eq({ errors: { email: ['Email has already been taken'] } }.to_json)
         end
       end
 
@@ -45,8 +51,11 @@ RSpec.describe Users::RegistrationsController do
 
         it 'throws the correct error' do
           do_action
-          expect(response.body).to eq({ errors: { email: ['can\'t be blank'],
-                                                  password: ['can\'t be blank'] } }.to_json)
+          expect(response.body).to eq({ errors: { email:      ['Email cannot be blank', 'Email invalid'],
+                                                  password:   ['Password cannot be blank'],
+                                                  first_name: ['First name cannot be blank'],
+                                                  last_name:  ['Last name cannot be blank']
+                                                } }.to_json)
         end
       end
     end
