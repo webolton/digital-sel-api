24cfd840bcdb3fc7b6b7b099afbe324950f6605e
William Bolton <william.ellet@gmail.com>
2018-07-22 14:03:36 -0400

    Fix cancan call in users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@
 
 class UsersController < ApplicationController
   before_action :authenticate_user!
-  load_and_authorize_resource only: %i[show update destroy]
+  load_and_authorize_resource
 
   def index
     users = User.all
