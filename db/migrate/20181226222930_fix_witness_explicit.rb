412e8cf0e724fe3ff992b37a77de45c6eb59f044
William Bolton <william.ellet@gmail.com>
2018-12-26 17:31:40 -0500

    Fix misnamed column

---
+++ db/migrate/20181226222930_fix_witness_explicit.rb
@@ -0,0 +1,5 @@
+class FixWitnessExplicit < ActiveRecord::Migration[5.2]
+  def change
+    rename_column :witnesses, :excipit, :explicit
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_26_221003) do
+ActiveRecord::Schema.define(version: 2018_12_26_222930) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -79,7 +79,7 @@ ActiveRecord::Schema.define(version: 2018_12_26_221003) do
     t.text "description"
     t.text "notes"
     t.string "incipit"
-    t.string "excipit"
+    t.string "explicit"
   end
 
 end
