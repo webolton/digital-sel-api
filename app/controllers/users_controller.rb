a4a76118683271a2d6ffa3297e64218d1ffb6977
William Bolton <william.ellet@gmail.com>
2019-01-07 20:37:11 -0500

    Fix user json shape

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,7 +18,6 @@ class UsersController < ApplicationController
 
   def create
     user = User.new(user_params)
-
     if user.save
       render json: { success: 'User created', user_id: user.id }, status: 201
     else
@@ -28,7 +27,7 @@ class UsersController < ApplicationController
 
   def update
     if @user.update(user_params)
-      render json: { success: 'User updated', user_id: @user.id }, status: 200
+      render json: { user: format_user(@user) }, status: 200
     else
       render_errors(@user)
     end
