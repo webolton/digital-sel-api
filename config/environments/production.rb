b11e99a2bbf5829f356b081586bd9329fe7ef64d
William Bolton <william.ellet@gmail.com>
2019-03-31 09:40:02 -0400

    Upgrade rails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -54,7 +54,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "digital-sel-api_#{Rails.env}"
+  # config.active_job.queue_name_prefix = "digital_sel_api_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
 
