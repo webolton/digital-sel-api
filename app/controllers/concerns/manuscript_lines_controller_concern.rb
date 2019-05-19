310e6753bcc113dee0abbbd0984e56bf2ca6c337
William Bolton <william.ellet@gmail.com>
2019-05-19 13:19:55 -0400

    Method for making folio-line numbers

--- app/controllers/concerns/manuscript_lines_controller_concern.rb
+++ app/controllers/concerns/manuscript_lines_controller_concern.rb
@@ -12,6 +12,18 @@ module ManuscriptLinesControllerConcern
     end
   end
 
+  def create_manuscript_line_number(sel_line_number, foliation, note = false)
+    foliation.each do |range_string, folio|
+      range = range_string.split('..').inject{ |st_range, end_range| st_range.to_i..end_range.to_i }
+      if range.include?(sel_line_number.to_i)
+        folio_line_no = range.find_index(sel_line_number.to_i) + 1
+        if note
+          return "#{folio}-#{folio_line_no}-marginal-note"
+        else
+          return "#{folio}-#{folio_line_no}"
+        end
+      end
+    end
   end
 
   def create_marginal_note(note, witness)
