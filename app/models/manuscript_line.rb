42191e740b2b54345798366bb1176ec0c794f581
William Bolton <william.ellet@gmail.com>
2019-05-19 14:54:32 -0400

    Validate uniqueness of ids

--- app/models/manuscript_line.rb
+++ app/models/manuscript_line.rb
@@ -4,4 +4,6 @@ class ManuscriptLine < ApplicationRecord
   belongs_to :witness
 
   validates :witness, presence: true
+  validates :sel_id, uniqueness: true
+  validates :ms_line_number, uniqueness: true
 end
