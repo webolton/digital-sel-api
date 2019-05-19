80dc88c7409d720edaad9c18a449bff622a5bfe7
William Bolton <william.ellet@gmail.com>
2019-05-19 13:21:21 -0400

    Improved line creator

--- app/controllers/concerns/manuscript_lines_controller_concern.rb
+++ app/controllers/concerns/manuscript_lines_controller_concern.rb
@@ -40,18 +40,27 @@ module ManuscriptLinesControllerConcern
     return true if note_keys.map{ |key| key.split('.').first }.include?(line_number)
   end
 
-  def create_line(line, index, witness, dictionary, foliation)
+  def notes_for_line(line_number, notes)
+    notes.select{ |key, val| key.split('.').first == line_number }
+  end
+
+  def create_line(line, index, witness, dictionary, foliation, notes)
     ms_line = ManuscriptLine.new
     ms_line.witness = witness
     ms_line.witness_line_number = index + 1
-    ms_line.sel_id = "#{witness.manuscript.siglum}_#{witness.saints_legend.siglum}_#{ms_line.witness_line_number}"
+    ms_line.sel_id = "#{witness.manuscript.siglum}-#{witness.saints_legend.siglum}-#{ms_line.witness_line_number}"
+    ms_line.ms_line_number = create_manuscript_line_number(ms_line.witness_line_number, foliation)
     ms_line.transcribed_line = line
-    formatted_line = line
+    expanded_line = line
 
     dictionary.to_hash.each do |abbrev|
-      formatted_line.gsub!(abbrev[0], abbrev[1])
+      expanded_line.gsub!(abbrev[0], abbrev[1])
     end
 
-    "<div>#{formatted_line}</div>"
+    if line_note?(ms_line.witness_line_number, notes.keys)
+      puts format_line(expanded_line, ms_line, notes_for_line(ms_line.witness_line_number, notes))
+    else
+     # format_line(expanded_line, ms_line)
+    end
   end
 end
