0e4d4a0e3b39685ea95bb171a8fc6071e0edd2a2
William Bolton <william.ellet@gmail.com>
2019-05-26 13:01:24 -0400

    Return witnesses with saints_legends

--- app/controllers/saints_legends_controller.rb
+++ app/controllers/saints_legends_controller.rb
@@ -3,7 +3,10 @@
 class SaintsLegendsController < ApplicationController
 
   def index
-    @saints_legends = SaintsLegend.all
-    render json: { saints_legends: @saints_legends.as_json(except: %i[created_at updated_at]) }
+    @saints_legends = SaintsLegend.all.includes(:witnesses).as_json(
+      except: %i[created_at updated_at],
+      include: [witnesses: { except: %i[created_at updated_at] }]
+    )
+    render json: { saints_legends: @saints_legends }
   end
 end
