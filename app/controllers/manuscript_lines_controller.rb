fd581f1bb6a35f5cfb786318e6ca495191020426
William Bolton <william.ellet@gmail.com>
2019-06-16 12:16:22 -0400

    Raise test for missing nested parameters

--- app/controllers/manuscript_lines_controller.rb
+++ app/controllers/manuscript_lines_controller.rb
@@ -6,11 +6,17 @@ class ManuscriptLinesController < ApplicationController
   authorize_resource
 
   def create
-    ms_siglum = manuscript_line_params['manuscript_line']['ms_siglum']
-    legend_siglum = manuscript_line_params['manuscript_line']['saints_legend_siglum']
-    dictionary = manuscript_line_params['manuscript_line']['dictionary']
+    ms_siglum     = manuscript_line_params['ms_siglum']
+    legend_siglum = manuscript_line_params['saints_legend_siglum']
+    dictionary    = manuscript_line_params['dictionary']
+    foliation     = manuscript_line_params['foliation']
+
+    if [ms_siglum, legend_siglum, dictionary, foliation].any?(&:nil?)
+      render_errors(['All Manuscript Lines params required'], 400)
+      return
+    end
+
     witness = Witness.for_ms_and_siglum(ms_siglum, legend_siglum)
-    foliation = manuscript_line_params['manuscript_line']['foliation']
 
     transcription_path = Dir.glob(
       Rails.root.join('docs', 'transcriptions', ms_siglum.downcase, legend_siglum, '*.seltxt')
